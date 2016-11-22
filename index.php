48a4771887b734c4dff246b52bf737faed087681
alexander <alexander@alexander>
2016-11-22 17:24:56 +0200

    new Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once 'core/init.php';
+
+if(Session::exists('home')) {
+    echo '<p>'. Session::flash('home') .'</p>';
+}
+
+$user = new User;
+if($user->isLoggedIn()) {
+?>
+    <p>Hello <a href="#"><?php echo escape($user->data()->username);?></a>!</p>
+
+    <ul>
+        <li>
+            <a href="logout.php">Log out</a>
+            <a href="update.php">Update details</a>
+        </li>
+
+    </ul>
+<?php
+}else {
+    echo '<p>You need to <a href="login.php">log in</a> or <a href="register.php">register</a></p>';
+}
+
